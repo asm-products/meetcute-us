a614505b79f8ef8776bdde1fc54c6922e787e701
jonlunsford <jon@capturethecastle.net>
2014-02-12 15:51:29 -0800

    WIP user subscriptions specs

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -14,7 +14,12 @@ describe "Users", :type => :feature do
 
     it "should have a link to their account info" do
       visit users_path(user)
-      expect(page).to have_content "Account info"
+      expect(page).to have_content "My account"
+    end
+
+    it "should have a link to their subscription info" do
+      visit users_path(user)
+      expect(page).to have_content "My subscription"
     end
 
   end

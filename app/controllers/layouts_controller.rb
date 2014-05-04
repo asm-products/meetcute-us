fa6ea8cde56d97502e24ce3ef1f4db3abd0f1c67
Jon Lunsford <jon@capturethecastle.net>
2014-05-04 13:23:04 -0700

    added layouts_controller and specs

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -1,22 +1,52 @@
 class LayoutsController < ApplicationController
+  
   def index
+    @layouts = Layout.all
+  end
+
+  def show
+    @layout = Layout.find(params[:id])
   end
 
   def new
+    @layout = Layout.new
   end
 
   def create
+    @layout = Layout.create(layout_params)
+    if @layout.save
+      flash[:notice] = "Layout created successfully."
+      redirect_to layout_path @layout
+    else
+      render :new
+    end
   end
 
   def edit
+    @layout = Layout.find(params[:id])
   end
 
   def update
-  end
-
-  def delete
+    @layout = Layout.find(params[:id])
+    if @layout.update_attributes(layout_params)
+      flash[:notice] = "Layout updated successfully."
+      redirect_to layout_path @layout
+    else 
+      render :edit
+    end
   end
 
   def destroy
+    layout = Layout.find(params[:id])
+    if layout.destroy
+      flash[:notice] = "Layout deleted successfully."
+      redirect_to layouts_path
+    end
+  end
+
+  private
+
+  def layout_params
+    params.require(:layout).permit(:name, :description, :site_id)
   end
 end
--- spec/factories/layouts.rb
+++ spec/factories/layouts.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :layout do
     name {Faker::Name.first_name}
     description {Faker::Lorem.paragraph}
+    site_id {Faker::Number.digit}
   end
 end

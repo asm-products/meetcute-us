72f4430a3044a142dae30e42c0893f5d13da412c
jonlunsford <jon@capturethecastle.net>
2014-05-07 13:35:24 -0700

    added a few seed records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Design.create(name: "Single Page 1", description: "A single page layout with a full screen image at the top.")
+Plan.create(amount: 14, interval: "month", name: "Bronze Plan", currency: "usd", stripe_id: "bronze")

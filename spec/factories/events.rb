7a32a730749c53e8569ee2608e3db2b9d224dd4e
Jon Lunsford <jon@capturethecastle.net>
2014-05-20 20:12:55 -0700

    fixed events factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     date "2014-05-08 14:31:22"
     title {Faker::Name.first_name}
     description {Faker::Lorem.paragraph}
-    component_id {Faker::Number.digit}
+    site_id {Faker::Number.digit}
   end
 
   factory :invalid_event, parent: :event do |f|

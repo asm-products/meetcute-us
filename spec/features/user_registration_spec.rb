9de4ce3538972e54d03763ff86cb44138116d476
Jon Lunsford <jon@capturethecastle.net>
2014-07-27 12:06:51 -0700

    Fix registration spec

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -102,14 +102,15 @@ describe "User registration", :type => :feature do
     end
 
     it "allows a user to upate their info" do
-      fill_in "user[email]", :with => "test2@test.com"
-      fill_in "user[password]", :with => user.password
-      fill_in "user[password_confirmation]", :with => user.password
-      fill_in "user[current_password]", :with => user.password
-      click_button "Update"
+      within('#edit_user') do
+        fill_in "user[email]", :with => "test2@test.com"
+        fill_in "user[password]", :with => user.password
+        fill_in "user[password_confirmation]", :with => user.password
+        fill_in "user[current_password]", :with => user.password
+        click_button "Update"
+      end
       expect(page).to have_content "You updated your account successfully."
       expect(User.last.email).to eq("test2@test.com")
     end
   end
-
 end

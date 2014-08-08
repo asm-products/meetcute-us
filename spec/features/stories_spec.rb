7379d4f7bf96c7e222b2722858e333aa4bf33b5b
Jon Lunsford <jon@capturethecastle.net>
2014-08-07 18:37:39 -0700

    Add stories nav item

--- spec/features/stories_spec.rb
+++ spec/features/stories_spec.rb
@@ -8,13 +8,26 @@ describe "Site stories", :type => :feature do
   after { Warden.test_reset! }
 
   context "Viewing stories" do
-	before do
-	  @story = create(:story, site: user.site)
-	  visit stories_path
-	end
+    before do
+      @story = create(:story, site: user.site)
+      visit stories_path
+    end
 
-	it "should list all stories" do
-	  expect(page).to have_content(@story.title)
-	end
+    it "should list all stories" do
+      expect(page).to have_content(@story.title)
+    end
+  end
+
+  context "Adding stories" do
+    before { new_story_path }
+
+    it "Should allow a Story to be added" do
+      fill_in "story[title]", :with => "My story tittle"
+      fill_in "story[content]", :with => "My content"
+      fill_in "story[display_order]", :with => 1
+      click_button "Add Story"
+      expect(page).to have_content "Your story has been created!"
+      expect(user.site.stores.last.title).to eq("My story title")
+    end
   end
 end

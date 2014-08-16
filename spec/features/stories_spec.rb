ffa5df228e86a4d74c9e5c28b466e5533f9457c5
Jon Lunsford <jon@capturethecastle.net>
2014-08-15 21:36:51 -0700

    Add stories shared form

--- spec/features/stories_spec.rb
+++ spec/features/stories_spec.rb
@@ -19,15 +19,32 @@ describe "Site stories", :type => :feature do
   end
 
   context "Adding stories" do
-    before { new_story_path }
+    before { visit new_story_path }
 
     it "Should allow a Story to be added" do
-      fill_in "story[title]", :with => "My story tittle"
+      fill_in "story[title]", :with => "My story title"
       fill_in "story[content]", :with => "My content"
       fill_in "story[display_order]", :with => 1
       click_button "Add Story"
       expect(page).to have_content "Your story has been created!"
-      expect(user.site.stores.last.title).to eq("My story title")
+      expect(user.site.stories.last.title).to eq("My story title")
     end
   end
+
+  context "Editing stories" do
+    before do
+      @story = create(:story, site: user.site)
+      visit edit_story_path(@story)
+    end
+
+    it "should all a story to be updated" do
+      fill_in "story[title]", :with => "My story title"
+      fill_in "story[content]", :with => "My content"
+      fill_in "story[display_order]", :with => 1
+      click_button "Update Story"
+      expect(page).to have_content "Your story has been created!"
+      expect(user.site.stories.last.title).to eq("My story title")
+    end
+  end
+
 end
